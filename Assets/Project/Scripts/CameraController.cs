b7839b29e4d81bb66e929208d338f1d99932b940
glibshchur <36221261+glibshchur@users.noreply.github.com>
2018-04-29 21:47:09 +0300

    Project Demo

---
+++ Assets/Project/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform[] views;
+	public float transitionSpeed; 
+	Transform currentView;
+	int i = 0;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	public void View0(){
+		i--;
+		if (i < 0) {
+			i = 3;
+		}
+		currentView = views [i];
+	}
+
+	public void View1(){
+		i++;
+		if (i > 3) {
+			i = 0;
+		}
+
+		currentView = views [i];	
+	}
+
+	void Update(){
+
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			currentView = views [0];
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			currentView = views [1];
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			currentView = views [2];
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha4)) {
+			currentView = views [3];
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha5)) {
+			currentView = views [4];
+		}
+
+	}
+
+
+	void LateUpdate () {
+
+		//Lerp position
+		transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+		Vector3 currentAngle = new Vector3 (
+			Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+			Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+			Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+		transform.eulerAngles = currentAngle;
+
+	}
+}
